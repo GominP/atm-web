e0ea41ec3d5672b67605d6e45bfe20f6f91852c9
gominpawantao <gomin.p@ku.th>
2020-09-20 19:06:40 +0700

    fix header backaccount to bankaacount

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -2,7 +2,13 @@ package th.ac.ku.atm.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import th.ac.ku.atm.model.BankAccount;
+import th.ac.ku.atm.service.BankAccountService;
 
 @Controller
 @RequestMapping("/bankaccount")
@@ -12,5 +18,18 @@ public class BankAccountController {
 
     public BankAccountController(BankAccountService bankAccountService) {
         this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping
+    public String getBankAccountPage(Model model) {
+        model.addAttribute("allBankAccounts",bankAccountService.getBankAccounts());
+        return "bankaccount";
+    }
+
+    @PostMapping
+    public String registerBankAccount(@ModelAttribute BankAccount bankAccount, Model model) {
+        bankAccountService.createBankAccount(bankAccount);
+        model.addAttribute("allBankAccounts", bankAccountService.getBankAccounts());
+        return "redirect:bankaccount";
     }
 }
